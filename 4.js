60af61f52cab27b2f75342efcbec42851b24c920
akbarism <59020048+akbarism@users.noreply.github.com>
2020-01-18 21:33:29 +0700

    Update 4.js

--- 4.js
+++ 4.js
@@ -1,31 +1,23 @@
-function findSame(arr=[]) {
-	var anagrams1 = [];
-	var anagrams2 = new Array();
-	for (let i=0; i<arr.length; i++) {
-		var word1 = arr[i];
-		var word2 = word1.match(/[a-zA-Z]/g).sort();
+function cetak_gambar(ganjil){
+    if( ganjil % 2 === 0){
+        console.log('Parameter harus angka dan ganjil!');
+        return;
+    }
 
-		if (anagrams1[word2] != null) {
-			anagrams1[word2].push(word1);
-		}
-		else {
-			anagrams1[word2] = [word1];
-		}
-	}
-
-	for (var key in anagrams1) {
-		if (anagrams1[key].length !== 1) {
-			anagrams2.push(anagrams1[key].join(", "));
-		}
-	}
-
-	if (anagrams2.length === 0) {
-		return console.log("Tidak ditemukan!");
-	}
-	else {
-		return console.log(anagrams2.join("\n"));
-	}
-}
-
-findSame(["cat", "listen", "code", "act", "silent", "tac"]);
-findSame(["try", "fire", "dark"]);
+    let str = "";
+    let nilaiTengah = Math.floor(ganjil / 2);
+    
+    for (let i = 0; i < ganjil; i++) {
+        for (let j = 0; j < ganjil; j++) {
+            if (j === 0 || i === nilaiTengah || j === ganjil - 1) {
+                str += '* ';
+            } else {
+                str += '= ';
+            }
+        }
+        str += '\n';
+    }
+    console.log(str);
+};
+cetak_gambar(5);
+cetak_gambar(4);
